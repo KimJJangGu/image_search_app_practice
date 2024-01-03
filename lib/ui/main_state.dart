fd22bb912002500a60840cecbd8524ade5899015
Kim DoHyeon <dohyun51731@gmail.com>
2024-01-03 22:46:44 +0900

    freezed 추가

--- lib/ui/main_state.dart
+++ lib/ui/main_state.dart
@@ -1,55 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_search_app_practice/data/model/image.dart';
 
-class MainState {
-  final List<ImageModel> imageModel;
+part 'main_state.freezed.dart';
+part 'main_state.g.dart';
 
-  final bool isLoading;
+@freezed
+class MainState with _$MainState {
+  const factory MainState({
+    @Default([]) List<ImageModel> imageItems,
+    @Default(false) bool isLoading,
+  }) = _MainState;
 
-//<editor-fold desc="Data Methods">
-  const MainState({
-    required this.imageModel,
-    required this.isLoading,
-  });
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      (other is MainState &&
-          runtimeType == other.runtimeType &&
-          imageModel == other.imageModel &&
-          isLoading == other.isLoading);
-
-  @override
-  int get hashCode => imageModel.hashCode ^ isLoading.hashCode;
-
-  @override
-  String toString() {
-    return 'MainState{ imageModels: $imageModel, isLoading: $isLoading,}';
-  }
-
-  MainState copyWith({
-    List<ImageModel>? imageModels,
-    bool? isLoading,
-  }) {
-    return MainState(
-      imageModel: imageModels ?? this.imageModel,
-      isLoading: isLoading ?? this.isLoading,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'imageModels': imageModel,
-      'isLoading': isLoading,
-    };
-  }
-
-  factory MainState.fromMap(Map<String, dynamic> map) {
-    return MainState(
-      imageModel: map['imageModels'] as List<ImageModel>,
-      isLoading: map['isLoading'] as bool,
-    );
-  }
-
-//</editor-fold>
+  factory MainState.fromJson(Map<String, Object?> json) => _$MainStateFromJson(json);
 }
